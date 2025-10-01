9e4ab5db1ed982fb60355e8a34078b97cd91eed4
sonice007 <soni.setiawan.it07@gmail.com>
2025-10-01 20:40:13 +0700

    fix any

--- services/product.service.ts
+++ services/product.service.ts
@@ -41,6 +41,43 @@ export const productApi = apiSlice.injectEndpoints({
       }),
     }),
 
+    getProductListPublic: builder.query<
+      {
+        data: Product[];
+        last_page: number;
+        current_page: number;
+        total: number;
+        per_page: number;
+      },
+      { page: number; paginate: number }
+    >({
+      query: ({ page, paginate }) => ({
+        url: `/public/products`,
+        method: "GET",
+        params: {
+          page,
+          paginate,
+        },
+      }),
+      transformResponse: (response: {
+        code: number;
+        message: string;
+        data: {
+          current_page: number;
+          data: Product[];
+          last_page: number;
+          total: number;
+          per_page: number;
+        };
+      }) => ({
+        data: response.data.data,
+        last_page: response.data.last_page,
+        current_page: response.data.current_page,
+        total: response.data.total,
+        per_page: response.data.per_page,
+      }),
+    }),
+
     // 🔍 Get Product by Slug
     getProductBySlug: builder.query<Product, string>({
       query: (slug) => ({
@@ -106,6 +143,7 @@ export const productApi = apiSlice.injectEndpoints({
 
 export const {
   useGetProductListQuery,
+  useGetProductListPublicQuery,
   useGetProductBySlugQuery,
   useCreateProductMutation,
   useUpdateProductMutation,
